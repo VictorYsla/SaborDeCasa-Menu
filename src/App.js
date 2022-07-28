2825bb847d9a2ee7edbd52d3ab4c3af186863169
Victor Fernando Gallardo Ysla <victorysla98@gmail.com>
2022-07-27 21:56:31 -0500

    change heigth checkpoint

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: "#f9934d" }}>
       <img
         src={require("../src/menu/7b17a35b-cc94-4567-9d6f-fd685ea56173.jpeg")}
-        alt="car"
+        // alt="car"
+        style={{ height: "100vh", width: "100%" }}
       />
     </div>
   );
