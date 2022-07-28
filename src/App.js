d9f0e69d0ed0007d18d25e6b5d4e80f66fa60b52
Victor Fernando Gallardo Ysla <victorysla98@gmail.com>
2022-07-27 23:34:49 -0500

    fixes responsive image checkpoint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 
 function App() {
+  let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
   return (
     <div
       className="App"
@@ -12,7 +14,10 @@ function App() {
       <img
         src={require("../src/menu/7b17a35b-cc94-4567-9d6f-fd685ea56173.jpeg")}
         alt="car"
-        style={{ height: "80%", width: "100%" }}
+        style={{
+          height: "80%",
+          width: isMobile ? "100%" : undefined,
+        }}
       />
       <a
         href="https://maps.app.goo.gl/B3zmn9fbsAtaBsiF7"
