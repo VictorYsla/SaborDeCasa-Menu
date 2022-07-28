3af744ac1722c061f1904411d7c73863f787fda1
Victor Fernando Gallardo Ysla <victorysla98@gmail.com>
2022-07-27 22:58:24 -0500

    change background color

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App" style={{ height: "100vh" }}>
+    <div
+      className="App"
+      style={{
+        backgroundColor: "#ff914f",
+        height: "100vh",
+      }}
+    >
       <img
         src={require("../src/menu/7b17a35b-cc94-4567-9d6f-fd685ea56173.jpeg")}
         alt="car"
-        style={{ height: "90%", width: "100%" }}
+        style={{ height: "80%", width: "100%" }}
       />
       <a
         href="https://maps.app.goo.gl/B3zmn9fbsAtaBsiF7"
