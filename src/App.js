748fb281ed77c74ed74bd95360fac00416169293
Victor Fernando Gallardo Ysla <victorysla98@gmail.com>
2022-07-27 22:03:42 -0500

    modified menu

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App" style={{ backgroundColor: "#f9934d" }}>
+    <div className="App">
       <img
         src={require("../src/menu/7b17a35b-cc94-4567-9d6f-fd685ea56173.jpeg")}
-        // alt="car"
+        alt="car"
         style={{ height: "100vh", width: "100%" }}
       />
     </div>
